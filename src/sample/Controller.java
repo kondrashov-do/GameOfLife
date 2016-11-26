081e302c2d233c1cc3f09c4c63467b187ccd4b68
Kondrashov <dkondras@campus.tue.nl>
2016-11-26 15:34:31 +0100

    create root inside Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,36 @@
 package sample;
 
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
 public class Controller {
+
+     public GridPane createGrid()
+     {
+         GridPane root = null;
+         try {
+             root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+             //init alive cells
+
+             createKillCell(root);
+
+         //
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         return root;
+     }
+
+     public void createKillCell(GridPane pane)
+     {
+         Text cellName = new Text("Cell"); // [Column] [Row]
+         Text cellName2 = new Text("Cell");
+         pane.add(cellName, 0, 0);
+         pane.add(cellName2, 0, 8);
+     }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,7 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 public class Main extends Application {
 
@@ -12,14 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        GridPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        Text cellName = new Text("Cell"); // [Column] [Row]
-        root.add(cellName, 0, 0);
-        root.getChildren().remove(cellName);
+       // root.getChildren().remove(cellName);
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.setTitle("Hello Game Of Life");
+        primaryStage.setScene(new Scene(new Controller().createGrid(), 1000, 1000));
         primaryStage.show();
 
     }
