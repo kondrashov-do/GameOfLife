0662615f196705ae20514f043fc80c44edab2557
Kondrashov <dkondras@campus.tue.nl>
2016-11-26 17:31:20 +0100

    running through 0 row

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -24,34 +24,40 @@ public class Controller {
             //
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return root;
     }
 
-    public void createField(GridPane pane) {
+    public void createField(GridPane pane) throws InterruptedException {
 
         int length = 10;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
+        for (int i = 0; i < length; i++) { // row
+            for (int j = 0; j < length; j++) { // column
                 // Create a new rectangle in each Iteration
                 rectangles[i][j] = new Rectangle(99,99,Color.WHITE);
-                pane.setColumnIndex(rectangles[i][j], i);
-                pane.setRowIndex(rectangles[i][j], j);
+                rectangles[i][j].setFill(Color.WHITE);
+                pane.setRowIndex(rectangles[i][j], i);
+                pane.setColumnIndex(rectangles[i][j], j);
                 pane.getChildren().add(rectangles[i][j]);
             }
         }
-        addCell(pane, 0,0);
-        //for (int j = 0; j < length; j++) {
-        //    if (pane.getChildren().) {
+        rectangles[0][0].setFill(Color.RED);
+        for (int j = 0; j < length-1; j++) {
+            if (rectangles[0][j + 1].getFill() == Color.WHITE  ) {
+                rectangles[0][j + 1].setFill(Color.RED);
+                rectangles[0][j].setFill(Color.WHITE);
+            }
 
-        //    }
-        //}
+        }
 
 
     }
 
     public void removeCell(GridPane pane, int column, int row) {
         Rectangle rectangle = new Rectangle(99,99,Color.WHITE);
+
         pane.setColumnIndex(rectangle, column);
         pane.setRowIndex(rectangle, row);
         pane.getChildren().add(rectangle);
