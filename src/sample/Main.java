6c7c4d6e1ad6310802402b47e407d83e7b74838f
Kondrashov <dkondras@campus.tue.nl>
2016-11-26 14:58:54 +0100

    something nice created, looks like gridPane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,20 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
 
+        GridPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Text cellName = new Text("Cell"); // [Column] [Row]
+        root.add(cellName, 0, 0);
+        root.getChildren().remove(cellName);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
